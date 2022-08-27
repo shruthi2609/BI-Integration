7a21a2b7e05b2108e3aae454c60a5a4526fe2276
shruthi093 <jayasruthi.l@gmail.com>
2022-08-28 00:57:28 +0530

    6000

--- models/TaskManagerModel.js
+++ models/TaskManagerModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TaskManager')
+mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://cram_js:cramjs@cluster0.zdsqd.mongodb.net/?retryWrites=true&w=majority')
     .then((res) => console.log('Connected to database'))
     .catch((err) => console.log('Error in connection'))
 
